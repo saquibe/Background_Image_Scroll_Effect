f235f79b3ef98cdbd4240177f9fad64036871685
Mohammad Saquib <saquibe143@gmail.com>
2024-09-14 20:00:24 +0530

    add functionality of the project

---
+++ index.js
@@ -0,0 +1,10 @@
+const bgImageEl = document.getElementById("bg-image");
+
+window.addEventListener("scroll", ()=> {
+    updateImage();
+});
+
+function updateImage(){
+    bgImageEl.style.opacity = 1 - window.scrollY / 800;
+    bgImageEl.style.backgroundSize = 160 - window.scrollY / 12 + "%";
+}
